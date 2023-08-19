97e17ac862599ebf27f6bc14a4453263448c53f8
pavbyte <pavbyte@gmail.com>
2023-08-19 10:51:36 +0530

    added secured route using auth0

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const { requiresAuth } = require('express-openid-connect');
 
 router.get("/", (req, res) => {
-    console.log(req.oidc.isAuthenticated());
-    res.render("index", { title: "Express" });
+    // console.log(req.oidc.isAuthenticated());
+    res.render("index",
+        {
+            title: "Express",
+            isAuthenticated: req.oidc.isAuthenticated(),
+            user: req.oidc.user,
+        });
 });
 
-module.exports = router;
+router.get("/secured", requiresAuth(), (req, res) => {
+    res.render("secured",
+        {
+            title: "secured section",
+            isAuthenticated: req.oidc.isAuthenticated(),
+            user: req.oidc.user,
+        });
+});
+
+module.exports = router;    
